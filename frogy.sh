0025470a1240566f1af75844d6461e373a717f51
Chintan Gurjar <chintangurjar@outlook.com>
2021-03-23 10:01:51 +0000

    Update frogy.sh

--- frogy.sh
+++ frogy.sh
@@ -22,7 +22,7 @@ echo -e "
 
 ############################################################### Housekeeping tasks ######################################################################
 
-echo -e "\e[94mEnter the organisation name (without space): \e[0m"
+echo -e "\e[94mEnter the organisation name (For space include '+', E.g. recorded+future): \e[0m"
 read org
 echo -e "\e[94mEnter the root domain name (eg: frogy.com): \e[0m"
 read domain_name
@@ -44,7 +44,6 @@ else
 fi
 
 ############################################################### Subdomain enumeration ######################################################################
-
 echo -e "\e[92mIdentifying Subdomains \e[0m"
 
 echo -n "Is this program is in CHAOS dataset? (y/n)? "
@@ -85,6 +84,22 @@ cat temp_wordlist.txt | anew | sed '/^$/d' | sed 's/\*\.//g' | grep -v " " | gre
 rm temp_wordlist.txt
 mv $org-wordlist.txt output/$org
 
+DIR="$PWD/crt"
+mkdir $DIR
+curl -s https://crt.sh/?q=$domain_name > $DIR/curl.txt
+cat $DIR/curl.txt | grep ?id= | cut -d \" -f5 | cut -d '<' -f1 | cut -d '>' -f2 >> $DIR/crt.ids
+cat $DIR/crt.ids| while read line
+do
+   curl -s https://crt.sh/?id=$line > $DIR/$line.txt
+done
+cat $DIR/* | grep -oP '(DNS)(.*?)(<BR>)' | cut -d ":" -f2 | cut -d "<" -f1 | sort -u >> output/$org/crt.txtls
+sed -ie 's/*.//g' output/$org/crt.txtls
+#rm -r crt
+cat output/$org/crt.txtls >> all.txtls
+echo -e "\e[36mCertificate search count: \e[32m$(cat output/$org/crt.txtls | anew | wc -l)\e[0m"
+
+echo -e "\e[36mCertificate search count: \e[32m$(cat output/$org/crt.txtls | anew | wc -l)\e[0m"
+
 python3 sublister/sublist3r.py -d $domain_name -o sublister_output.txt &> /dev/null
 if [[ -e sublister_output.txt ]]
 then
@@ -105,20 +120,18 @@ cat output/$org/dnscan.txtls | grep $org | egrep -iv ".(DMARC|spf|=|[*])" | cut 
 
 echo -e "\e[36mDnscan: \e[32m$(cat output/$org/dnscan.txtls | anew | wc -l)\e[0m"
 
-awk -F\. '{print $(NF-1) FS $NF}' all.txtls | anew >> final.txtls
-subfinder -dL final.txtls --silent >> output/$org/subfinder2.txtls
+grep -P "^.[^.]+\.[a-zA-Z]{3}$|^.[^.]+\.[a-zA-Z]{2}\.[a-zA-Z]{2}$" all.txtls | anew  >> tld.txtls
+subfinder -dL tld.txtls --silent >> output/$org/subfinder2.txtls
 echo -e "\e[36mSubfinder count: \e[32m$(cat output/$org/subfinder2.txtls | anew | wc -l)\e[0m"
+cat output/$org/subfinder2.txtls | grep "/" | cut -d "/" -f3 >> all.txtls
+cat output/$org/subfinder2.txtls | grep -v "/" >> all.txtls
 
-cat output/$org/subfinder2.txtls >> all.txtls
-
-rm final.txtls
+mv tld.txtls output/$org/
 echo "www.$domain_name" >> all.txtls
 echo "$domain_name" >> all.txtls
-cat all.txtls | grep $org | anew >> $org.master
+cat all.txtls | anew | grep -v "*." >> $org.master
 mv $org.master output/$org/$org.master
+sed -i 's/<br>/\n/g' output/$org/$org.master
 rm all.txtls
-echo -e "\e[93mTotal UNIQUE subdomains found: $(cat output/$org/$org.master | anew | wc -l)\e[0m"
-
-echo -e "\e[94mGenerating live URLs...\e[0m"
-cat output/$org/$org.master | httpx --silent -ports 80,280,443,591,593,832,981,1965,2480,4444,4445,4567,5104,5800,5985,5986,7000,7002,8008,8042,8080,8088,8222,8243,8280,8281,8403,8448,8530,8531,8887,8888,9443,9981,11371,12043,12046,12443,16080,18091,18092 >> output/$org/liveurls.txtls
-echo -e "\e[93mCount of live websites: $(cat output/$org/liveurls.txtls | anew | wc -l)\e[0m"
+echo -e "\e[93mTotal UNIQUE subdomains found: $(cat output/$org/$org.master | wc -l)\e[0m"
+echo -e "\e[93mTotal UNIQUE TLDs found: $(cat output/$org/tld.txtls | wc -l)\e[0m"
