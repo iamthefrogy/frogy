0a2335f0220ec4f20130573cf0e70618f0b25660
Chintan Gurjar <chintangurjar@outlook.com>
2021-10-30 12:20:22 +0100

    Update frogy.sh

--- frogy.sh
+++ frogy.sh
@@ -141,6 +141,8 @@ cat all.txtls | anew | grep -v "*." >> $org.master
 mv $org.master output/$org/$org.master
 sed -i 's/<br>/\n/g' output/$org/$org.master
 rm all.txtls
-echo -e "\e[93mTotal UNIQUE subdomains found: $(cat output/$org/$org.master | wc -l)\e[0m"
-echo -e "\e[93mTotal UNIQUE TLDs found: $(cat output/$org/tld.txtls | wc -l)\e[0m"
+httpx -l output/$org/$org.master -p 8080,10000,20000,2222,7080,9009,7443,2087,2096,8443,4100,2082,2083,2086,9999,2052,9001,9002,7000,7001,8082,8084,8085,8010,9000,2078,2080,2079,2053,2095,4000,5280,8888,9443,5800,631,8000,8008,8087,84,85,86,88,10125,9003,7071,8383,7547,3434,10443,8089,3004,81,4567,7081,82,444,1935,3000,9998,4433,4431,4443,83,90,8001,8099,80,300,443,591,593,832,981,1010,1311,2480,3128,3333,4243,4711,4712,4993,5000,5104,5108,6543,7396,7474,8014,8042,8069,8081,8088,8090,8091,8118,8123,8172,8222,8243,8280,8281,8333,8500,8834,8880,8983,9043,9060,9080,9090,9091,9200,9800,9981,12443,16080,18091,18092,20720,28017 -o output/$org/livesites.txtls &> /dev/null
+echo -e "\e[93mTotal live websites (on all available ports) found: \e[32m$(cat output/$org/livesites.txtls | wc -l)\e[0m"
+echo -e "\e[93mTotal UNIQUE subdomains found: \e[32m$(cat output/$org/$org.master | wc -l)\e[0m"
+echo -e "\e[93mTotal UNIQUE TLDs found: \e[32m$(cat output/$org/tld.txtls | wc -l)\e[0m"
 cat output/$org/tld.txtls
