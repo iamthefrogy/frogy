d98d41735d3b55087a4d3842f41114a26d16e879
Chintan Gurjar <chintangurjar@outlook.com>
2021-11-10 21:07:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
-apt install jq
-apt install whois
+apt install -y jq
+apt install -y whois
+sudo apt install -y libpcap-dev
 chmod +x frogy.sh
 git clone https://github.com/aboul3la/Sublist3r.git
 git clone https://github.com/rbsec/dnscan.git
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go get -u github.com/tomnomnom/anew
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
+chmod +x findomain-linux
+cp findomain-linux /usr/bin
+cd /root/go/bin
+cp anew httpx subfinder naabu /usr/bin/
