16fbbd6773d64dcf479b9b56a10fe8e9860f81c2
Chintan Gurjar <chintangurjar@outlook.com>
2021-11-10 00:20:38 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-apt install jq  &> /dev/null
-apt install whois  &> /dev/null
-git clone https://github.com/iamthefrogy/frogy.git  &> /dev/null
-cd frogy  &> /dev/null
-chmod +x frogy.sh  &> /dev/null
-git clone https://github.com/aboul3la/Sublist3r.git  &> /dev/null
-git clone https://github.com/rbsec/dnscan.git  &> /dev/null
+apt install jq
+apt install whois
+chmod +x frogy.sh
+git clone https://github.com/aboul3la/Sublist3r.git
+git clone https://github.com/rbsec/dnscan.git
