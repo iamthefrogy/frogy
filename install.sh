29ec5bd6adeede8382d2fe2bed46fdbc2084cc88
Chintan Gurjar <chintangurjar@outlook.com>
2022-01-03 10:55:27 +0000

    Changed to go install instead of go get

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ chmod +x frogy.sh
 git clone https://github.com/aboul3la/Sublist3r.git
 git clone https://github.com/rbsec/dnscan.git
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-go get -u github.com/tomnomnom/anew
+go install -v github.com/tomnomnom/anew@latest
+go install -v github.com/tomnomnom/waybackurls@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
