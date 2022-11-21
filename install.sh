42fbba1420c6fb80ebae0628bd34122964ae305b
Devender <49671176+raoshaab@users.noreply.github.com>
2022-11-21 19:05:08 +0530

    Removed Go lang dependency

--- install.sh
+++ install.sh
@@ -1,26 +1,95 @@
 ##### RUN IT AS ROOT AND ENSURE YOU HAVE GO INSTALLED ON YOUR KALI MACHINE
+if [[ whoami != root ]]
+then 
+    echo -e "\033[1;91m"
+    echo -e "\t\t\t!! Run with sudo !! " 
+    exit 
+fi
 
 apt update
-apt install -y jq
-apt install -y whois
-apt install -y libpcap-dev
-apt install -y golang
+apt-get -qq install pip jq whois amass libpcap-dev -y 
 chmod +x frogy.sh
 git clone https://github.com/rbsec/dnscan.git
-go install -v github.com/tomnomnom/anew@latest
-go install -v github.com/tomnomnom/waybackurls@latest
-go install github.com/tomnomnom/unfurl@latest
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-apt install -y amass
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-wget https://github.com/Findomain/Findomain/releases/download/8.2.1/findomain-linux.zip
-unzip findomain-linux.zip
-chmod +x findomain
-mv findomain /usr/bin/
-rm findomain-linux.zip
-cd /root/go/bin
-cp anew httpx waybackurls subfinder unfurl /usr/bin/
-# kali user and ubuntu user
-apt install pip
-git clone https://github.com/rbsec/dnscan.git
 pip install -r dnscan/requirements.txt
+
+function project_discovery(){
+    case $(arch) in         
+    x86_64)
+        cpu='amd64'
+        ;;
+    i686 | i386)
+        #calling i386 function
+        cpu='386'
+        ;;
+    aarch64)
+        #calling aarch function
+        cpu='arm64'
+        ;;
+    *)
+        cpu='amd64'
+        ;;
+    esac
+
+    for tool in {httpx,subfinder}
+    do
+        version=$(curl -IkLs -o /dev/null -w %{url_effective}  https://github.com/projectdiscovery/$tool/releases/latest|grep -o "[^/]*$"| sed "s/v//g")
+        baseurl="https://github.com/projectdiscovery/$tool/releases/download/v$version/"$tool"_$version"_linux_"$cpu.zip"
+        wget -q $baseurl -O $tool.zip && unzip -q $tool.zip '$tool' 
+        chmod +x $tool && mv $tool /usr/bin/ && rm $tool.zip
+    done
+
+}
+
+function tomnomnom(){
+    case $(arch) in         
+    x86_64)
+        cpu='amd64'
+        ;;
+    i686 | i386)
+        cpu='386'
+        ;;
+    *)
+        cpu='amd64'
+        ;;
+    esac
+
+    for tool in {anew,waybackurls,unfurl}
+    do
+        version=$(curl -IkLs -o /dev/null -w %{url_effective}  https://github.com/tomnomnom/$tool/releases/latest|grep -o "[^/]*$"| sed "s/v//g")
+        baseurl="https://github.com/tomnomnom/$tool/releases/download/v$version/$tool-linux-$cpu-$version.tgz"
+                
+        wget -q $baseurl -O $tool.tgz && tar xf $tool.tgz  
+        chmod +x $tool && mv $tool /usr/bin/ && rm $tool.tgz
+    done
+}
+
+
+function find_domain(){
+    case $(arch) in         
+    x86_64)
+        cpu='-linux'
+        ;;
+    i686 | i386)
+        #calling i386 function
+        cpu='-linux-i386'
+        ;;
+    aarch64)
+        #calling aarch function
+        cpu='-aarch64'
+        ;;
+    *)
+        cpu='-linux'
+        ;;
+    esac
+    findomain_version=$(curl -IkLs -o /dev/null -w %{url_effective}  https://github.com/Findomain/Findomain/releases/latest|grep -o "[^/]*$"| sed "s/v//g")
+    baseurl="https://github.com/Findomain/Findomain/releases/download/$findomain_version/findomain$cpu.zip"
+    wget -q $baseurl -O findomain.zip && unzip -q findomain.zip && rm findomain.zip 
+    chmod +x findomain && mv findomain /usr/bin/ 
+}
+
+#Calling functions to downlaod binary file & move to /usr/bin
+project_discovery
+tomnomnom
+find_domain
+
+
